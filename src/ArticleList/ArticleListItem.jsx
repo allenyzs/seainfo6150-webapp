5bbe74fe8686948514f06f520208c7f78d22b66d
Zhuoshu Yang <zhuoshuyang@Zhuoshus-MacBook-Pro.local>
2020-02-18 14:56:07 -0800

    week-6

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import styles from "./ArticleListItem.module.css";
 import SlugButton from "./SlugButton";
+import ArticleImage from "./ArticleImage"
 
 const ArticleListItem = props => {
   return (
     <div className={styles.container}>
-      <h3 className={styles.title}>{props.article.title}</h3>
-      <p>{props.article.shortText}</p>
-      <time className={styles.pubDate} dateTime={props.article.pubYear}>{props.article.pubDate}</time>
-      <SlugButton slug={props.article.slug} buttonText={props.article.author} />
+      <div className={styles.image}>
+        <ArticleImage url={props.article.image._url} title={props.article.title}></ArticleImage>
+      </div>
+      <div>
+        <h3 className={styles.title}>{props.article.title}</h3>
+        <p>{props.article.shortText}</p>
+        <time className={styles.pubDate} dateTime={props.article.pubYear}>{props.article.pubDate}</time>
+      </div>
+      <div>
+      <SlugButton className={styles.button} slug={props.article.slug} buttonText={props.article.author} />
+      </div>
+      <div className={styles.clear}></div>
     </div>
   );
 };
